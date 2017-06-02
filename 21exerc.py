99f784c24dfbb6a03cadc7eebf852b09b6297a31
Nelson Nunes <nelsonjr86@gmail.com>
2017-06-02 15:47:15 -0300

    Add files via upload

---
+++ 21exerc.py
@@ -0,0 +1,40 @@
+import mincemeat
+import glob
+import csv
+
+text_files = glob.glob('textos\\*')
+
+def file_contents(file_name):
+    f = open(file_name)
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+source = dict((file_name, file_contents(file_name))for file_name in text_files)
+
+def mapfn(k,v):
+    print 'map ' + k
+    from stopwords import allStopWords
+    for line in v.splitlines():
+        for word in line.split():
+            yield word, 1
+
+def reducefn(k,v):
+    print 'reduce ' + k
+    return v
+
+s = mincemeat.Server()
+
+# A fonte de dados pode ser qualquer objeto do tipo dicionario
+s.datasource = source
+s.mapfn = mapfn
+s.reducefn = reducefn
+
+results = s.run_server(password="changeme")
+
+w = csv.writer(open("Result.csv", "w"))
+for k, v in results.items():
+    w.writerow([k, v])
+
+
